99a107880f070012ebbdf0fa4c7080fad660705a
Administrator <2075530188@qq.com>
2020-09-20 16:39:27 +0800

    third commit homework

--- app/src/main/java/com/example/layout/AppMain.java
+++ app/src/main/java/com/example/layout/AppMain.java
@@ -5,7 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class AppMain extends AppCompatActivity {
-
+    /**sfsdf
+     *
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
