293b78ead4e8ba025d8efdcc30975b94958b76f8
Administrator <2075530188@qq.com>
2020-09-20 16:19:38 +0800

    third commit homework

--- app/src/main/java/com/example/layout/AppMain.java
+++ app/src/main/java/com/example/layout/AppMain.java
@@ -10,6 +10,6 @@ public class AppMain extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app_main);
-    }
+
 
 }
